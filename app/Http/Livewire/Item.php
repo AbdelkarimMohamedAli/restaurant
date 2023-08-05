58061f679597f78e723c049f3ed936de546d08e6
wbdao <abdulla15982@gmail.com>
2023-08-05 12:48:42 +0300

    uploade items medel

--- app/Http/Livewire/Item.php
+++ app/Http/Livewire/Item.php
@@ -2,31 +2,237 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Addon;
+use App\Models\Category;
 use App\Models\Item as ModelsItem;
+use App\Models\Matrial;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 use Livewire\WithPagination;
+use Nette\Utils\Random;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class Item extends Component
 {
+    use WithFileUploads;
     use WithPagination;
     protected $paginationTheme='bootstrap';
 
     public $formstatus=true;
     public $toggleModel=false;
-    public $names,$discreption,$item_id,$price;
+    public $names,$discreption,$item_id,$price,$category=0,$discount,$discountType,$image,$imgurl,$isImage=false;
+    public $variant,$option,$optionPrice;
+    public $variants=[],$addons_s=[],$matrial_s=[];
+    public $addonselect=false,$matrialSelect=false;
+    public $search='',$Msearch='';
+    
+    protected $rules=[
+        'names'=>'required',
+        'image' => 'required|mimes:png,jpg,jpeg,csv,pdf|max:2048',
+        'category'=>'required',
+        'discreption'=>'required',
+        'price'=>'required'
+    ];
     public function createform() {
         $this->formstatus=true;
         $this->toggleModel='show';
+        $this->isImage=true;
+        $this->reseti();
     }
     public function closeform() {
         $this->toggleModel='';
     }
+    // render Componenets
     public function render()
     {
+        // if($this->image){    
+        //     dd($this->image);
+        // }
+        if ($this->search){
+        $addons= Addon::where('name', 'like', '%' . $this->search . '%')->get();
+        }else{
+        $addons=Addon::all();
+        }
+        if($this->Msearch){
+            $matrials=Matrial::where('name', 'like', '%' . $this->Msearch . '%')->get();
+        }else{
+            $matrials=Matrial::all();
+        }
+        $categories=Category::all();
         $items=ModelsItem::query()->orderBy('updated_at','desc')->paginate(5);
         return view('livewire.item',[
-            'items'=>$items
+            'items'=>$items,
+            'addons'=>$addons,
+            'categories'=>$categories,
+            'matrials'=>$matrials
         ]);
         
+    }
+    public function update($prop){
+        if ($prop == 'search') {
+            $this->resetPage();
+        }
+    }
+    // open and close addons dropdown
+    public function addonSelectO(){
+        $this->addonselect=true;
+    }
+    public function addonSelectC(){
+        $this->addonselect=false;
+    }
+    // open and close matrials dropdown 
+    public function matrialSelectO(){
+        $this->matrialSelect=true;
+    }
+    public function matrialSelectC(){
+        $this->matrialSelect=false;
+    }
+     // add& remove Addons //
+    public function addAdons($id){
+        $addone=Addon::query()->find($id);
+        $addon=[
+            'id'=>$addone->id,
+            'name'=>$addone->name,
+            'price'=>$addone->price,
+        ];
+        array_push($this->addons_s,$addon);
+    }
+    
+    public function removeAddons($id){
+        unset($this->addons_s[$id]);
+    }
+    // add& remove Matrial
+    public function addMatrial($id){
+        $matriale=Matrial::query()->find($id);
+        $matrial=[
+            'id'=>$matriale->id,
+            'name'=>$matriale->name,
+            'price'=>$matriale->price,
+        ];
+        array_push($this->matrial_s,$matrial);
+    }
+    public function removeMatrial($id){
+        unset($this->matrial_s[$id]);
+    }
+    // add&remove varinat
+    public function addVariants(){
+        if ($this->variant){
+            array_push($this->variants,['name'=>$this->variant,'array'=>[]]);  
+        }
+        
+        // dd($this->variants);
+        $this->variant='';
+    }
+    public function removeVarint($id){
+        unset($this->variants[$id]);
+    }
+    public function addoptions($id){
+        $data=[
+            'name'=>$this->option,
+            'price'=>$this->optionPrice,
+        ];
+        array_push($this->variants[$id]['array'],$data);
+        
+    }
+    public function removeoptions($id,$id2){
+        unset($this->variants[$id]['array'][$id2]);
+    }
+    // add remove items
+    public function reseti() {
+        $this->names='';
+        $this->category=0;
+        $this->image='';
+        $this->discount='';
+        $this->discountType='';
+        $this->price='';
+        $this->addons_s=[];
+        $this->variants=[];
+        $this->matrial_s=[];
+        $this->discreption='';
+    }
+    public function addItems(){
+        
+        $this->validate();
+        // $this->imgurl = $this->image->getClientOriginalName();
+        // $this->image->store('files', 'public');
+        $imageName=Random::generate(10).'.'.$this->image->extension();
+        $this->image->storeAs('image_uploads', $imageName);
+        
+        ModelsItem::create([
+            'name'=>$this->names,
+            'category_id'=>$this->category,
+            'img'=>$imageName,
+            'discount'=>$this->discount,
+            'discount_type'=>$this->discountType,
+            'user_id'=>Auth::user()->id,
+            'price'=>$this->price,
+            'addons'=>$this->addons_s,
+            'variant'=>$this->variants,
+            'matrials'=>$this->matrial_s,
+            'description'=>$this->discreption,
+        ]);
+        
+        session()->flash('message', 'تمت الاضافه بنجاح');
+        $this->toggleModel='';
+        $this->reseti();
+    }
+    public function updateItem($id){
+        $item=ModelsItem::find($id);
+        $item->name= $this->names;
+        $item->category_id=$this->category;
+        $item->discount=$this->discount;
+        $item->discount_type=$this->discountType;
+        $item->price=$this->price;
+        $item->addons=$this->addons_s;
+        $item->variant=$this->variants;
+        $item->matrials=$this->matrial_s;
+        $item->description=$this->discreption;
+        $item->save();
+        // alert()->success('Title','Lorem Lorem Lorem');
+        session()->flash('message', 'تمت التعديل بنجاح');
+        $this->toggleModel='';
+        $this->reseti();
+    }
+    public function deleteItem($id){
+        $item=ModelsItem::find($id);
+        if(file_exists('uploads/image_uploads/'.$item->img)){
+            unlink('uploads/image_uploads/'.$item->img);
+          }
+        // unlink(public_path('image_uploads/'. $item->img));
+        $item->delete();
+        session()->flash('message', 'تمت الحذف بنجاح');
+    }
+    public function select($id){
+        
+        $item=ModelsItem::find($id);
+        if($item){
+            $this->names=$item->name;
+            $this->category=$item->category_id;
+            $this->image=$item->img;
+            $this->discount=$item->discount;
+            $this->discountType=$item->discount_type;
+            $this->price=$item->price;
+            $this->addons_s=$item->addons;
+            $this->variants=$item->variant;
+            $this->matrial_s=$item->matrials;
+            $this->discreption=$item->description;
+            $this->item_id=$item->id;
+        }
+        $this->formstatus=false;
+        $this->toggleModel='show';
+        $this->isImage=false;
+    }
+    public function select2($id){
+        
+        $item=ModelsItem::find($id);
+        if($item){
+            $this->names=$item->name;
+            $this->item_id=$item->id;
+        }
+        $this->dispatchBrowserEvent('show-form',['modalId'=>'#form','actionModal'=>'show']);
+    }
+    public function stauts(){
+        $this->isImage=true;
     }
 }
--- resources/views/livewire/item.blade.php
+++ resources/views/livewire/item.blade.php
@@ -7,6 +7,11 @@
             {{ session('message') }}
         </div>
     @endif
+    @if (session()->has('error'))
+        <div class="alert alert-error">
+            {{ session('error') }}
+        </div>
+    @endif
     <div class="card mt-5">
         <div class="card-body d-flex justify-content-between align-conteny-coen">
             <!-- modal-cover -->
@@ -22,9 +27,9 @@
         </div>
         
     </div>
-    <div class="offcanvas offcanvas-end {{$toggleModel}}"  data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel" style="min-width:450px">
+    <div class="offcanvas offcanvas-end {{$toggleModel}}"  data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel" style="width:750px; max-width:750px">
         <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Colored with scrolling</h5>
+            <h5 class="offcanvas-title float-end" id="offcanvasScrollingLabel">إضافة صنف جديد</h5>
             <button type="button" class="btn-close text-reset" wire:click='closeform' data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
@@ -33,29 +38,43 @@
                 <form class="was-validated"   method="post">
                     @csrf
                     <div class="row mb-n3">
-                        <div class="col-md-12 mb-3">
+                        <div class="col-md-6 mb-3">
                             <label for="validationInvalidInput" class="form-label float-end">اسم الصنف</label>
                             <input type="text" wire:model="names" class="form-control is-invalid" id="validationInvalidInput" value="" required>
                             @error('names') <span class="error text-danger float-end">{{ $message }}</span> @enderror
                         </div>
-                        <div class="col-md-12 mb-3">
+                        <div class="col-md-6 mb-3">
                             <label for="validationInvalidInput" class="form-label float-end">الوصف</label>
                             <input type="text" wire:model="discreption" class="form-control is-invalid" id="validationInvalidInput" value="" required>
                             @error('discreption') <span class="error text-danger float-end">{{ $message }}</span> @enderror
                         </div>
-                        <div class="col-md-12 mb-3">
+                        <div class="col-md-6 mb-3">
                             <label for="validationInvalidInput" class="form-label float-end">السعر</label>
-                            <input type="text" wire:model="" class="form-control is-invalid" id="validationInvalidInput" value="" required>
-                            @error('discreption') <span class="error text-danger float-end">{{ $message }}</span> @enderror
+                            <input type="text" wire:model="price" class="form-control is-invalid" id="validationInvalidInput" value="" required>
+                            @error('price') <span class="error text-danger float-end">{{ $message }}</span> @enderror
                         </div>
-                        <div class="col-md-12 mb-3">
-                            @if($formstatus)
-                            <button type="submit" wire:click.prevent='addItem' class="btn btn-outline-theme btn-lg w-100">إضافة الصنف</button>
-                            @else
-                            <button type="submit" wire:click.prevent='updateCategory()' class="btn btn-outline-theme btn-lg w-100">تعديل الصنف</button>
-                            @endif
+                        <div class="col-md-6 mb-3">
+                            <label for="validationSelectInvalid" class="form-label">اختر القسم</label>
+                            <select class="form-select is-invalid" id="validationSelectInvalid" wire:model="category" required>
+                                <option selected disabled value="0">Choose...</option>
+                                @foreach ($categories as $category)
+                                <option value="{{$category->id}}">{{$category->name}}</option>    
+                                @endforeach
+                            </select>
+                            @error('category') <span class="error text-danger">{{ $message }}</span> @enderror
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="validationInvalidInput" class="form-label float-end">الخصم</label>
+                            <input type="text" wire:model="discount" class="form-control is-invalid" id="validationInvalidInput" value="" required>
+                            @error('discount') <span class="error text-danger float-end">{{ $message }}</span> @enderror
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="validationInvalidInput" class="form-label float-end">نوع الخصم</label>
+                            <input type="text" wire:model="discountType" class="form-control is-invalid" id="validationInvalidInput" value="" required>
+                            @error('discountType') <span class="error text-danger float-end">{{ $message }}</span> @enderror
                         </div>
                     </div>
+                    
                 </form>
                 </div>
                 <div class="card-arrow">
@@ -65,7 +84,165 @@
                     <div class="card-arrow-bottom-right"></div>
                 </div>
                 <div class="hljs-container">
-                    <pre><code class="xml" data-url="assets/data/form-elements/code-14.json"></code></pre>
+                    <pre><code class="xml" data-url="assets/data/table-elements/code-3.json"></code></pre>
+                    <div class="card-header d-flex align-items-center justify-content-between  ">
+                        Variant
+                        
+                    </div>
+                    <div class="row mb-3 gx-3">
+                        <div class="col-12 mt-3">
+                            <ul id="tag-size" class="tagit form-control ui-widget ui-widget-content ui-corner-all">
+                                @if(is_array($variants))
+                                @foreach ($variants as $key=>$options)
+                                    <li class="tagit-choice ui-widget-content ui-state-default ui-corner-all tagit-choice-editable"><span class="tagit-label">{{$options['name']}}</span>
+                                        <a class="tagit-close" wire:click='removeVarint({{$key}})'><span class="text-icon">×</span><span class="ui-icon ui-icon-close"></span></a>
+                                    </li> 
+                                @endforeach
+                                @endif
+                                <li class="tagit-new w-100">
+                                    <input type="text" class="ui-widget-content ui-autocomplete-input" wire:model='variant' wire:keydown.enter='addVariants'>
+                                </li>
+                            </ul> 
+                        </div>
+                        <div>
+                            @if(is_array($variants))
+                            @foreach ($variants as $key=>$options)
+                            <div class="row my-3">
+                                <div class="col-4"><input type="text" class="form-control" value="{{$options['name']}}" placeholder="please Enter Varinat" readonly></div>
+                                <div class="col-8 row">
+                                    <div class="col-5"><input type="text" class="form-control" wire:model='option' placeholder="please Enter Option"></div>
+                                    <div class="col-5"><input type="text" class="form-control" wire:model='optionPrice' placeholder="please Enter Price"></div>
+                                    <div class="col-2">
+                                        <a href="#" class="btn btn-outline-theme d-block" wire:click='addoptions({{$key}})'>
+                                            <i class="fas fa-lg fa-fw me-2 fa-plus" ></i></a>
+                                    </div>
+                                    @foreach ($options['array'] as $key2=>$itams)
+                                    <div class="col-5">{{$itams['name']}}</div>
+                                    <div class="col-5">{{$itams['price']}}</div>
+                                    <div class="col-2">
+                                        <a href="#" class="btn  d-block" wire:click='removeoptions({{$key}},{{$key2}})'>
+                                        <i class="far fa-lg fa-fw me-2 fa-window-close" ></i></a>
+                                    </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                            @endforeach
+                            @endif
+                        </div>
+                    </div>
+                    <div class="card-header d-flex align-items-center ">
+                        الاضافات
+                    </div>
+                    
+                    <div class="row mb-3 gx-3">
+                        <div class="col-12">
+                            <ul id="tag-size" class="tagit form-control ui-widget ui-widget-content ui-corner-all">
+                                
+                                @foreach ($addons_s as $key=>$addon)
+                                    <li class="tagit-choice ui-widget-content ui-state-default ui-corner-all tagit-choice-editable"><span class="tagit-label">{{$addon['name']}}</span>
+                                        <a class="tagit-close" wire:click='removeAddons({{$key}})'><span class="text-icon">×</span><span class="ui-icon ui-icon-close"></span></a>
+                                    </li> 
+                                @endforeach
+                               
+                                <li class="tagit-new w-100">
+                                    <input type="text" class="ui-widget-content ui-autocomplete-input" wire:model='search' wire:click='addonSelectO'>
+                                </li>
+                            </ul>
+                            <div class="z-100 w-100 addon-select-{{$addonselect?'o':'c'}}">
+                                <div class="card bg-gray-900 w-100">
+                                    <div class="card-header fw-bold small">
+                                        <button class="btn btn-outline-theme float-end " wire:click='addonSelectC'>close</button>
+                                    </div>
+                                    <div class="card-body" style="max-height: 300px; overflow:auto">
+                                        <div class="list-group">
+                                        @foreach ($addons as $addon)
+                                            <a href="#" class="list-group-item list-group-item-action" wire:click='addAdons({{$addon['id']}})'>
+                                                {{$addon['name']}}
+                                            </a>
+                                        @endforeach
+                                        </div>
+                                    </div>
+                                    <div class="card-arrow">
+                                        <div class="card-arrow-top-left"></div>
+                                        <div class="card-arrow-top-right"></div>
+                                        <div class="card-arrow-bottom-left"></div>
+                                        <div class="card-arrow-bottom-right"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="card-header d-flex align-items-center ">
+                        الخامات
+                    </div>
+                    <div class="row mb-3 gx-3">
+                        <div class="col-12">
+                            <ul id="tag-size" class="tagit form-control ui-widget ui-widget-content ui-corner-all">
+                                @if(is_array($matrial_s))
+                                @foreach ($matrial_s as $key=>$matrial)
+                                    <li class="tagit-choice ui-widget-content ui-state-default ui-corner-all tagit-choice-editable"><span class="tagit-label">{{$matrial['name']}}</span>
+                                        <a class="tagit-close" wire:click='removeMatrial({{$key}})'><span class="text-icon">×</span><span class="ui-icon ui-icon-close"></span></a>
+                                    </li> 
+                                @endforeach
+                                @endif
+                                <li class="tagit-new w-100">
+                                    <input type="text" class="ui-widget-content ui-autocomplete-input" wire:model='Msearch' wire:click='matrialSelectO'>
+                                </li>
+                            </ul>
+                            <div class="z-100 w-100 addon-select-{{$matrialSelect?'o':'c'}}">
+                                <div class="card bg-gray-900 w-100 " >
+                                    <div class="card-header fw-bold small">
+                                        <button class="btn btn-outline-theme float-end " wire:click='matrialSelectC'>close</button>
+                                    </div>
+                                    <div class="card-body" style="max-height: 300px; overflow:auto">
+                                        <div class="list-group">
+                                        @foreach ($matrials as $matrial)
+                                            <a href="#" class="list-group-item list-group-item-action" wire:click='addMatrial({{$matrial['id']}})'>
+                                                {{$matrial['name']}}
+                                            </a>
+                                        @endforeach
+                                        </div>
+                                    </div>
+                                    <div class="card-arrow">
+                                        <div class="card-arrow-top-left"></div>
+                                        <div class="card-arrow-top-right"></div>
+                                        <div class="card-arrow-bottom-left"></div>
+                                        <div class="card-arrow-bottom-right"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="hljs-container">
+                    <pre><code class="xml" data-url="assets/data/table-elements/code-3.json"></code></pre>
+                    <div class=" mb-3 d-flex align-items-center justify-content-between">
+                        <div>
+                        <input type="file" wire:model="image" class="form-control " wire:change='stauts'>
+                        </div>
+                        <div>
+                            @if ($image)
+                                @if ($isImage)
+                                    <img src="{{$image->temporaryUrl()}}" alt="" style="width:150px; height:150px" />
+                                @else
+                                    <img src="uploads/image_uploads/{{$image}}" alt="" width="100" height="100" />
+                                @endif
+                             
+                            @endif
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="hljs-container">
+                    <pre><code class="xml" data-url="assets/data/table-elements/code-3.json"></code></pre>
+                    <div class="col-md-12 mb-3">
+                        @if($formstatus)
+                        <button type="submit" wire:click.prevent='addItems' class="btn btn-outline-theme btn-lg w-100 ">إضافة الصنف</button>
+                        @else
+                        <button type="submit" wire:click.prevent='updateItem({{$item_id}})' class="btn btn-outline-theme btn-lg w-100">تعديل الصنف</button>
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
@@ -82,18 +259,23 @@
                         <th scope="col">السعر</th>
                         <th scope="col">الخصم</th>
                         <th scope="col">طبيعة الخصم</th>
+                        <th scope="col">الصورة</th>
+                        <th scope="col">Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($items as $item)
+                    @foreach ($items as $key=>$item)
                     <tr>
-                        <td scope="col">{{$item->id}}</td>
+                        <td scope="col">{{$key+1}}</td>
                         <td scope="col">{{$item->name}}</td>
                         <td scope="col">{{$item->category->name}}</td>
                         <td scope="col">{{$item->price}}</td>
                         <td scope="col">{{$item->discount}}</td>
                         <td scope="col">{{$item->discount_type}}</td>
-                        
+                        <td>
+                            <img src="/uploads/image_uploads/{{$item->img}}" alt="{{$item->name}}" width="100"
+                            height="100"/>                        
+                        </td>
                         <td>
                             <button class="btn btn-primary me-1" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" wire:click='select({{$item->id}})'>تعديل</button>
                             <button class="btn btn-danger me-1" wire:click='select2({{$item->id}})'>Delete</button>
@@ -110,10 +292,31 @@
             <div class="card-arrow-bottom-left"></div>
             <div class="card-arrow-bottom-right"></div>
         </div>
-        
+
         <div class="hljs-container">
             <pre><code class="xml" data-url="assets/data/table-elements/code-3.json"></code></pre>
             {{$items->links()}}
+        </div>
+    </div>
+    {{-- نافذة الحذف --}}
+    <div class="modal fade " id="form"  aria-modal="true" role="dialog">
+        <div class="modal-dialog modal-sm">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">هل انت متاكد نم عمليةالحذف</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-6">
+                            <button type="button" class="btn btn-danger btn-block btn-lg" wire:click='deleteItem({{$item_id}})' data-bs-dismiss="modal">Delete</button>
+                        </div>
+                        <div class="col-6">
+                            <button type="button" class="btn btn-outline-theme btn-block btn-lg" data-bs-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
